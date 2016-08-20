458635c544a5a016c8fc4a791ed474c537a91351
dprice06468 <thepricefamily@gmail.com>
2016-08-20 14:28:35 -0400

    initial checkin (not for moteino)

---
+++ moteinoRelayRx/moteinoRelayRx.ino
@@ -0,0 +1,31 @@
+//trigger a relay given serial input
+//send a $1 to Rx to close the relay
+//send a $2 to Rx to open the relay
+
+int pinRelay = 9;  //relay trigger pin
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  Serial.begin(9600);
+
+  // initialize digital pin 13 as an output.
+  pinMode(pinRelay, OUTPUT);
+  digitalWrite(pinRelay, HIGH);       // open the relay
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  if (Serial.available()>0)
+  {
+    char val = Serial.read();
+    if (val == '$') {
+      delay(50);
+      val = Serial.read();
+      Serial.println("found input: " + val);
+
+      if (val == '1') digitalWrite(pinRelay, LOW);   //close the relay
+      if (val == '2') digitalWrite(pinRelay, HIGH);  //open the relay
+      Serial.flush();
+    }
+  }
+}
